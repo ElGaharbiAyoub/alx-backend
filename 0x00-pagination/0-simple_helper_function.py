e53bfb0f650b97c375d765338cef6b8f07786657
elgharbi <elgharbiay@gmail.com>
2024-01-29 01:36:38 +0100

    fix pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 from typing import Tuple, List, Dict
 
 
-def index_range(page:int, page_size:int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Return a tuple of size two containing a start index and an end index
         corresponding to the range of indexes to return in a list for those
         particular pagination parameters.
