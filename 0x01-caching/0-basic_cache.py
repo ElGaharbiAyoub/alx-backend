766422e4e026e98d4c9f95e03593327975b39b74
elgharbi <elgharbiay@gmail.com>
2024-01-31 23:33:14 +0100

    done first taks

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+ #!/usr/bin/env python3
+"""Basic dictionary"""
+from base_caching import BaseCaching
+
+
+class BasicCache (BaseCaching):
+    """BasicCache class"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
