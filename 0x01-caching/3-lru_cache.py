0b675a07e0aa4cd71ac0ec7f441c901721f52046
elgharbi <elgharbiay@gmail.com>
2024-02-01 02:22:46 +0100

    Fix

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -9,7 +9,7 @@ class LRUCache (BaseCaching):
     def __init__(self):
         """Constructor"""
         super().__init__()
-        self.last = ""
+        self.last = []
 
     def put(self, key, item):
         """ Add an item in the cache"""
@@ -17,14 +17,19 @@ class LRUCache (BaseCaching):
             return
         if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
             if key not in self.cache_data.keys():
-                print("DISCARD: {}".format(self.last))
-                self.cache_data.pop(self.last)
+                print("DISCARD: {}".format(self.last[0]))
+                self.cache_data.pop(self.last[0])
+                self.last.pop(0)
         self.cache_data[key] = item
-        self.last = key
+        if key in self.last:
+            self.last.remove(key)
+        self.last.append(key)
 
     def get(self, key):
         """ get an item by key"""
         if key is None or key not in self.cache_data.keys():
             return None
-        self.last = key
+        if key in self.last:
+            self.last.remove(key)
+        self.last.append(key)
         return self.cache_data.get(key)
