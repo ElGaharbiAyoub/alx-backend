e81ac878361a64d48ec4a8f59ca0af69e6eeadea
elgharbi <elgharbiay@gmail.com>
2024-02-01 02:34:35 +0100

    FIX

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,34 +10,36 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """Constructor"""
         super().__init__()
-        self.last = []
         self.count = {}
-
-    def put(self, key, item):
-        """ Add an item in the cache"""
-        if key is None or item is None:
-            return
-        if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
-            if key not in self.cache_data.keys():
-                print("DISCARD: {}".format(self.last[0]))
-                self.cache_data.pop(self.last[0])
-                self.last.pop(0)
-        self.cache_data[key] = item
-        if key in self.last:
-            self.last.remove(key)
-        self.last.append(key)
-        if key not in self.count.keys():
-            self.count[key] = 0
-        self.count[key] += 1
-
-    def get(self, key):
-        """ get an item by key"""
-        if key is None or key not in self.cache_data.keys():
-            return None
-        if key in self.last:
-            self.last.remove(key)
-        self.last.append(key)
-        if key not in self.count.keys():
-            self.count[key] = 0
-        self.count[key] += 1
-        return self.cache_data.get(key)
+    
+        def put(self, key, item):
+            """ Add an item in the cache"""
+            if key is None or item is None:
+                return
+            if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data.keys():
+                    min_value = min(self.count.values())
+                    min_keys = [k for k in self.count if self.count[k] == min_value]
+                    if len(min_keys) == 1:
+                        print("DISCARD: {}".format(min_keys[0]))
+                        self.cache_data.pop(min_keys[0])
+                        self.count.pop(min_keys[0])
+                    else:
+                        for k in self.last:
+                            if k in min_keys:
+                                print("DISCARD: {}".format(k))
+                                self.cache_data.pop(k)
+                                self.count.pop(k)
+                                break
+            self.cache_data[key] = item
+            if key in self.count.keys():
+                self.count[key] += 1
+            else:
+                self.count[key] = 1
+    
+        def get(self, key):
+            """ get an item by key"""
+            if key is None or key not in self.cache_data.keys():
+                return None
+            self.count[key] += 1
+            return self.cache_data.get(key)
