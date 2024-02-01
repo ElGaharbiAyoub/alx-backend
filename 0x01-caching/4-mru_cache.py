21681412d4ddebba8fd76f89ae7a89af8119b12e
elgharbi <elgharbiay@gmail.com>
2024-02-01 02:25:37 +0100

    Add MRU Cache implementation and test in 4-main.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""MRU Caching"""
+from base_caching import BaseCaching
+
+
+class MRUCache (BaseCaching):
+    """MRUCache class"""
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.last = []
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data.keys():
+                print("DISCARD: {}".format(self.last[-1]))
+                self.cache_data.pop(self.last[-1])
+                self.last.pop(-1)
+        self.cache_data[key] = item
+        if key in self.last:
+            self.last.remove(key)
+        self.last.append(key)
+    
+    def get(self, key):
+        """ get an item by key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        if key in self.last:
+            self.last.remove(key)
+        self.last.append(key)
+        return self.cache_data.get(key)
