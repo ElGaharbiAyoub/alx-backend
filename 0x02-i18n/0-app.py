93b5ca911c9560f64053fbe657d321e30f62412c
elgharbi <elgharbiay@gmail.com>
2024-02-07 01:07:40 +0100

    FIX

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,3 +8,7 @@ app = Flask(__name__)
 def index():
     """ Returns the index page """
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
