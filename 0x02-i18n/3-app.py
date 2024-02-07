5574efb9bd9db099cfb5bb9b6702e68ea27ae075
elgharbi <elgharbiay@gmail.com>
2024-02-07 01:43:18 +0100

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,7 +26,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """ Returns the index page """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
